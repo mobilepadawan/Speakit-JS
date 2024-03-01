18140642e1f5b669cb5d0f7980b370315a3cf86b
Fernando <48518539+mobilepadawan@users.noreply.github.com>
2024-03-01 17:36:48 -0300

    Another update of the official website

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,5 +1,8 @@
 const emojiSun = document.querySelector('div.emoji.sun')
 const emojiMoon = document.querySelector('div.emoji.moon')
+const buttonRead = document.querySelector("#buttonRead")
+const inputText = document.querySelector("#inputText")
+const selectedLanguage = document.querySelector("#selectedLanguage")
 
 const applyThemePicked = ()=> localStorage.getItem('themePicked') || 'auto'
 
@@ -14,4 +17,9 @@ emojiSun.addEventListener('click', ()=> {
 emojiMoon.addEventListener('click', ()=> {
     document.documentElement.style.setProperty('color-scheme', 'Dark')
     localStorage.setItem('themePicked', 'Dark')
+})
+
+buttonRead.addEventListener("click", ()=> {
+    let textToRead = inputText.value.trim()
+    textToRead && Speakit.readText(textToRead, selectedLanguage.value)
 })
