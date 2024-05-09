9f04ef358af6c14abaf65dfb9fcbc3daa76fcfaa
Fernando <48518539+mobilepadawan@users.noreply.github.com>
2024-05-08 22:39:46 -0300

    Minimum fixes in Alpha version and first .min file

--- history/Speakit.alpha.1.1.0.js
+++ history/Speakit.alpha.1.1.0.js
@@ -1,5 +1,5 @@
 class Speakit extends SpeechSynthesis {
-    static utteranceRate = 1.01
+    static utteranceRate = 1.02
     static utterancePitch = 1.0
     static totalAvailableVoices = 0
     static languageFilter = ""    // U can specify here an ISO language to filter the voices
@@ -30,9 +30,7 @@ class Speakit extends SpeechSynthesis {
             Speakit.totalVoices = await this.#getAvailableVoices()
             // ADDED THE FOLLOWING LINES TO APPLY THE NEW PROPERTY FOR VOICES FILTERING PURPOSES
             if (Speakit.totalVoices.length > 0 && Speakit.languageFilter.trim() !== "") {
-              // let filteredLanguages = Speakit.totalVoices.filter((voice)=> voice.lang === Speakit.languageFilter.trim() )
-              // I propose to change the way to filter ISO languages by granting filtering with just a part of the ISO language.
-              // We will get more flexibility and a wide languages list to pick for.
+            //   let filteredLanguages = Speakit.totalVoices.filter((voice)=> voice.lang === Speakit.languageFilter.trim() )
               let filteredLanguages = Speakit.totalVoices.filter((voice)=> voice.lang.includes(Speakit.languageFilter.trim()) )
               if (filteredLanguages.length > 0) {
                 Speakit.totalVoices.length = 0
